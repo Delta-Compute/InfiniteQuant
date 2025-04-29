6e63193d7d51440fb0183d3aafd3f1714a08276c
unknown <suresh.sanghani9@gmail.com>
2025-04-29 21:39:46 +0530

    adding setup file

---
+++ template/protocol.py
@@ -0,0 +1,44 @@
+# The MIT License (MIT)
+# Copyright © 2024 Delta Prop Shop LLC
+# developer: Delta-Compute
+# Copyright © 2024 Delta Prop Shop LLC
+
+import typing
+import bittensor as bt
+from pydantic import Field
+
+from typing import List
+
+class SendSignal(bt.Synapse):
+    signal: typing.Dict = Field(default_factory=dict, title="Signal", frozen=False)
+    successfully_processed: bool = Field(False, title="Successfully Processed", frozen=False)
+    error_message: str = Field("", title="Error Message", frozen=False)
+    validator_hotkey: str = Field("", title="Hotkey set by validator", frozen=False)
+    order_json: str = Field("", title="New Order JSON set by validator", frozen=False)
+    miner_order_uuid: str = Field("", title="Order UUID set by miner", frozen=False)
+    computed_body_hash: str = Field("", title="Computed Body Hash", frozen=False)
+SendSignal.required_hash_fields = ["signal"]
+
+class GetPositions(bt.Synapse):
+    positions: List[typing.Dict] = Field(default_factory=list, title="Positions", frozen=False)
+    successfully_processed: bool = Field(False, title="Successfully Processed", frozen=False)
+    error_message: str = Field("", title="Error Message", frozen=False)
+    computed_body_hash: str = Field("", title="Computed Body Hash", frozen=False)
+    version: int = Field(0, title="Version", frozen=False)
+
+GetPositions.required_hash_fields = ["positions"]
+
+class ValidatorCheckpoint(bt.Synapse):
+    checkpoint: str = Field("", title="Checkpoint", frozen=False)
+    successfully_processed: bool = Field(False, title="Successfully Processed", frozen=False)
+    error_message: str = Field("", title="Error Message", frozen=False)
+    validator_receive_hotkey: str = Field("", title="Hotkey set by receiving validator", frozen=False)
+    computed_body_hash: str = Field("", title="Computed Body Hash", frozen=False)
+ValidatorCheckpoint.required_hash_fields = ["checkpoint"]
+
+class GetDashData(bt.Synapse):
+    data: typing.Dict = Field(default_factory=dict, title="Dashboard Data", frozen=False)
+    successfully_processed: bool = Field(False, title="Successfully Processed", frozen=False)
+    error_message: str = Field("", title="Error Message", frozen=False)
+    computed_body_hash: str = Field("", title="Computed Body Hash", frozen=False)
+GetDashData.required_hash_fields = ["data"]
